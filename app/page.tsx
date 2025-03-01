a4226ebf5d522ab1f1ee56fdcc6bca5bd09b54bd
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-02-28 21:04:36 -0300

    feat: create initial form

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,166 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+} from "./_components/ui/card";
+
+import { Label } from "./_components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./_components/ui/select";
+import { Input } from "./_components/ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "./_components/ui/button";
+import { FileInput } from "lucide-react";
+
+const framework = [
+  { name: "Cypress" },
+  { name: "Selenium" },
+  { name: "Robot Framework" },
+  { name: "Playwright" },
+  { name: "Jest" },
+  { name: "Mocha" },
+  { name: "TestCafe" },
+  { name: "Puppeteer" },
+  { name: "Appium" },
+  { name: "Katalon Studio" },
+  { name: "JUnit" },
+  { name: "Capybara" },
+  { name: "Sikuli" },
+  { name: "TestNG" },
+  { name: "Karate Framework" },
+];
+
+const schema = z.object({
+  framework: z.string().min(1, "Selecione um framework"),
+  name: z.string().min(1, "O campo nome é obrigatório"),
+  email: z.string().email("E-mail inválido").min(1, "E-mail é obrigatório"),
+  phone: z
+    .string()
+    .min(10, "Telefone deve ter pelo menos 10 dígitos")
+    .max(15, "Telefone não pode ter mais de 15 dígitos")
+    .optional(),
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const onSubmit = (data: any) => {
+    console.log("Adicionado com sucesso", data);
+    reset();
+  };
+
+  return (
+    <main className="min-h-screen flex items-start justify-center pt-20 bg-gray-200">
+      <Card className="w-[1024px] flex flex-col items-center justify-center p-6 rounded-2xl shadow-2xl border-2">
+        <CardHeader className="flex flex-col items-center gap-6">
+          <h1 className="text-4xl font-bold">Test Pick</h1>
+          <CardDescription>
+            <p className="text-lg">
+              Escolha qual o framework para automação você mais gosta de
+              utilizar
+            </p>
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid w-full items-center gap-6">
+              <div className="flex flex-col space-y-1.5">
+                <Label
+                  htmlFor="framework"
+                  className="items-center flex justify-center"
+                >
+                  Framework
+                </Label>
+                <Select onValueChange={(value) => setValue("framework", value)}>
+                  <SelectTrigger id="framework" className="w-55">
+                    <SelectValue placeholder="Selecione o framework" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    {framework.map((item, index) => (
+                      <SelectItem key={index} value={item.name}>
+                        {item.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                {errors.framework?.message && (
+                  <p className="text-red-500 text-xs text-center">
+                    {errors.framework.message as string}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="flex flex-col mt-4 mb-4 gap-4 items-center justify-center">
+              <Label htmlFor="name">Nome</Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="Informe seu nome"
+                {...register("name")}
+              />
+              {errors.name && (
+                <p className="text-red-500 text-xs text-center">
+                  {errors.name.message}
+                </p>
+              )}
+
+              <Label htmlFor="email">E-mail</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Informe seu e-mail"
+                {...register("email")}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-xs text-center">
+                  {errors.email.message}
+                </p>
+              )}
+
+              <Label htmlFor="phone">Telefone</Label>
+              <Input
+                id="phone"
+                type="number"
+                placeholder="Informe seu telefone"
+                {...register("phone")}
+              />
+              {errors.phone && (
+                <p className="text-red-500 text-xs text-center">
+                  {errors.phone.message}
+                </p>
+              )}
+            </div>
+
+            <Button
+              type="submit"
+              className="w-55 text-2xl h-10 gap-3 flex items-center justify-center"
+            >
+              <FileInput size={24} />
+              Enviar
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
